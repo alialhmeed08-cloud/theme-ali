85b3540712fd3f0ad2e0273db5c2b4a8c86c9656
alialhmeed08-cloud <alialhmeed08@gmail.com>
2026-08-19 18:26:42 +0300

    fix: webpack config supports SCSS + Tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,58 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const path = require('path');
 
-const asset     = file => path.resolve('src/assets', file || '');
+const asset = file => path.resolve('src/assets', file || '');
 const publicDir = file => path.resolve('public', file || '');
 
 module.exports = {
     entry: {
-        app:   [asset('styles/app.css'),   asset('js/app.js')],
-        pages: [asset('styles/pages.css'), asset('js/pages.js')]
+        app: [asset('styles/app.scss'), asset('js/app.js')],
+        pages: [asset('js/pages.js')]
     },
-    output: { path: publicDir(), clean: true },
+    output: {
+        path: publicDir(),
+        filename: '[name].js',
+        clean: true
+    },
+    resolve: {
+        extensions: ['.js', '.scss', '.css']
+    },
     module: {
         rules: [
-            { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, { loader: "css-loader", options: { url: false } }] }
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: { presets: ['@babel/preset-env'] }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { url: false } },
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { url: false } }
+                ]
+            }
         ]
     },
-    plugins: [ new MiniCssExtractPlugin() ]
+    optimization: {
+        minimizer: [
+            '...',
+            new CssMinimizerPlugin()
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: '[name].css' })
+    ]
 };
